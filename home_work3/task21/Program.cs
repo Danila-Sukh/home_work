4b32bf267739d1db436d1f97569c8a87f7f9c9c7
Danila Sukhorukov <suhorukovdanila@yandex.ru>
2022-08-15 14:17:42 +0300

    home work3

--- home_work3/task21/Program.cs
+++ home_work3/task21/Program.cs
@@ -1,12 +1,10 @@
 ﻿Console.WriteLine("Введите координаты первой точки по очереди");
-int X1=int.Parse (Console.ReadLine());
-int Y1=int.Parse (Console.ReadLine());
-int Z1=int.Parse (Console.ReadLine());
-
+int X1 = int.Parse(Console.ReadLine());
+int Y1 = int.Parse(Console.ReadLine());
+int Z1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите координаты второй точки по очереди");
-int X2=int.Parse (Console.ReadLine());
-int Y2=int.Parse (Console.ReadLine());
-int Z2=int.Parse (Console.ReadLine());
+int X2 = int.Parse(Console.ReadLine());
+int Y2 = int.Parse(Console.ReadLine());
+int Z2 = int.Parse(Console.ReadLine());
 double max = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2) + Math.Pow((Z2 - Z1), 2));
-
 Console.WriteLine(Math.Round(max, 2));
