3aa04dac577ee670b8d6339bba389424ecdff7da
Danila Sukhorukov <suhorukovdanila@yandex.ru>
2022-09-05 00:17:34 +0300

    home work9

---
+++ home_work9/task64/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (m > n) Console.WriteLine($"Первое число {m} больше второго {n}. Поиск натуральных чисел рассчитываеться от второго к первому");
+int result = Count(m, n);
+int Count(int m, int n)
+{
+    if (m == n && m > 0)
+    {
+        Console.Write($" {m} ");
+        return m;
+    }
+    else if (m < n && m > 0)
+    {
+        Console.Write($" {m} ");
+        return Count(m + 1, n);
+    }
+    else if (m > n) return Count(n, m);
+    else return Count(1, n);
+}
---
+++ home_work9/task68/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int n = int.Parse(Console.ReadLine()); ;
+int result = Function_Akkerman(m, n);
+Console.Write($"Функция Аккермана = {result} ");
+int Function_Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Function_Akkerman(m - 1, 1);
+    else return Function_Akkerman(m - 1, Function_Akkerman(m, n - 1));
+}
